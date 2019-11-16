e5e2097524347df6c64444f0c2f5760adc6a3aac
yn1323 <yn1323@gmail.com>
2019-11-17 00:15:59 +0900

    Vuexファイル

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
+// Lib imports
 import Vue from "vue";
 import Vuex from "vuex";
 
-Vue.use(Vuex);
+// Store functionality
+import actions from "./actions";
+import getters from "./getters";
+import mutations from "./mutations";
+import state from "./state";
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+Vue.use(Vuex);
+// Create a new store
+const store = new Vuex.Store({
+  actions,
+  getters,
+  mutations,
+  state
 });
+
+export default store;
