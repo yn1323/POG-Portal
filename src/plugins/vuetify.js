e9712efaf7600d6a21803f4756b57161c0a4010a
yn1323 <yn1323@gmail.com>
2019-12-01 17:56:56 +0900

    Vuetifyのthemeをstyle内で使用できるようにする

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import theme from "./theme";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.use(Vuetify, {
   iconfont: "fa"
 });
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    theme,
+    options: {
+      customProperties: true
+    }
+  }
+});
